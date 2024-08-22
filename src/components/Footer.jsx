d36487cfc1b3f24482ea922792cff3abc645c5d7
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-08-22 19:36:53 +0300

    add footer and form components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,38 @@
+import footerLogo from '../assets/footer-logo.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInstagram, faWhatsapp } from '@fortawesome/free-brands-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
+import '../styles/Footer.css';
+
+export default function Footer() {
+
+    let year = new Date().getFullYear();
+
+    return(
+        <footer>
+            <div className="container d-flex align-items-center justify-content-between">
+                <div className="brand-socials d-flex flex-column py-5">
+                    <img src={footerLogo} alt="Meiras Yachting" />
+                    <div className="socials d-flex flex-column mt-5 align-items-start">
+                        <a target='_blank' rel="noopener noreferrer" href="https://www.instagram.com/meirasyachting/"><FontAwesomeIcon icon={faInstagram} size='xl'/><span>meirasyachting</span></a>
+                        <a target='_blank' rel="noopener noreferrer" href="https://wa.me/+905444136378"><FontAwesomeIcon icon={faWhatsapp} size='xl'/><span>+90 (544) 413 63 78</span></a>
+                        <a target='_blank' rel="noopener noreferrer" href="mailto:meirasyachting@gmail.com"><FontAwesomeIcon icon={faEnvelope} size='xl'/><span>meirasyachting@gmail.com</span></a>
+                    </div>
+                </div>
+                <div className="site-map d-flex">
+                    <ul>
+                        <li><a href="/">Anasayfa</a></li>
+                        <li><a href="/yachts">Hakkımızda</a></li>
+                        <li><a href="/about">Yat Kiralama</a></li>
+                    </ul>
+                    <ul>
+                        <li><a href="/contact">Hizmetlerimiz</a></li>
+                        <li><a href="/contact">Brokerage</a></li>
+                        <li><a href="/contact">İletişim</a></li>
+                    </ul>
+                </div>
+            </div>
+            <span className='copyright'>Copyright © {year} - All rights reserved | designed by <a target='_blank' rel="noopener noreferrer" href="https://www.instagram.com/meirasdesign/">Meiras Design</a> | developed by <a target='_blank' rel="noopener noreferrer" href="https://github.com/irfansubasi">irfansubasi</a></span>
+        </footer>
+    );
+}
---
+++ src/components/Form.jsx
@@ -0,0 +1,49 @@
+import '../styles/Form.css';
+export default function Form() {
+    return(
+        <div className="contact-form">
+            <div className="form-head p-5">
+                <h3>BİLGİ AL</h3>
+            </div>
+            <div className="form-container bg-white p-5 w-100">
+                <form action="" className='p-5'>
+                    <div className="row">
+                        <div className="col-md-6 mb-4">
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="nameandsurname" placeholder="Ad Soyad" required/>
+                                <label htmlFor="nameandsurname" className="form-label">Ad Soyad</label>
+                            </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="phone" placeholder="Cep Telefonu" required/>
+                                <label htmlFor="phone">Cep Telefonu</label>
+                            </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                            <div className="form-floating">
+                                <input type="email" className="form-control" id="mail" placeholder="E-mail" required/>
+                                <label htmlFor="mail">E-mail</label>
+                            </div>
+                        </div>
+                        <div className="col-md-6 mb-4">
+                            <div className="form-floating">
+                                <input type="text" className="form-control" id="city" placeholder="Şehir" required/>
+                                <label htmlFor="city">Şehir</label>
+                            </div>
+                        </div>
+                        <div className="col-md-12 mb-4">
+                            <div className="form-floating">
+                                <textarea className="form-control" id="message" placeholder="Mesaj" required></textarea>
+                                <label htmlFor="message">Mesaj</label>
+                            </div>
+                        </div>
+                        <div className="col-auto">
+                            <button type="submit" className="btn btn-primary">Gönder</button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    );
+}
