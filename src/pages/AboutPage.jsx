4ab181234709412b5167935462943a27dd1e64b0
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-08-24 19:39:47 +0300

    add styles and components to about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -14,33 +14,31 @@ export default function About() {
             <div className="merged">
                 <Header page="about"/>
                 <main>
-                    <section className="about-us">
-                        <div className="container">
+                    <section>
+                        <div className="container about-us">
                             <div className="about-container">
-                                <div className="about-section p-5">
+                                <div className="about-section">
                                     <img className="about-logo img-fluid" src={verticalLogo} alt="meiras" />
-                                    <div className="explain d-flex flex-column align-items-center">
+                                    <div className="m-auto explain d-flex flex-column align-items-center">
                                         <h3>Hakkımızda</h3>
                                         <p className="lead">Meiras Yatçılık olarak, Türkiye’nin en gözde yat kiralama destinasyonlarından biri olan Marmaris, Bodrum ve Göcek&apos;te sizlere unutulmaz bir deniz deneyimi sunmaktayız. Deniz tutkunlarına, lüks ve konforu bir araya getiren özel yat kiralama hizmetlerimizle, Ege ve Akdeniz&apos;in eşsiz güzelliklerini keşfetme fırsatı sunuyoruz.</p>
                                     </div>
                                 </div>
                                 <div className="about-socials">
-                                    <h4 className="border-bottom">Bize Ulaşmak İçin</h4>
+                                    <h4 className="border-bottom border-2">Bize Ulaşmak İçin</h4>
                                     <div className="social-box">
-                                        <div className="social-box-single">
-                                            <a href="https://www.instagram.com/meirasyachting/" target="_blank" rel="noreferrer">
-                                                <FontAwesomeIcon icon={faInstagram} />
-                                                <span>meirasyachting</span>
-                                            </a>
-                                            <a href="https://wa.me/+905444136378/" target="_blank" rel="noreferrer">
-                                                <FontAwesomeIcon icon={faWhatsapp} />
-                                                <span>+90 (544) 413 63 78</span>
-                                            </a>
-                                            <a href="mailto:meirasyachting@gmail.com" target="_blank" rel="noreferrer">
-                                                <FontAwesomeIcon icon={faEnvelope} />
-                                                <span>+90 (544) 413 63 78</span>
-                                            </a>
-                                        </div>
+                                        <a href="https://www.instagram.com/meirasyachting/" target="_blank" rel="noreferrer">
+                                            <FontAwesomeIcon size="2x" icon={faInstagram} />
+                                            <span>meirasyachting</span>
+                                        </a>
+                                        <a href="https://wa.me/+905444136378/" target="_blank" rel="noreferrer">
+                                            <FontAwesomeIcon size="2x" icon={faWhatsapp} />
+                                            <span>+90 (544) 413 63 78</span>
+                                        </a>
+                                        <a href="mailto:meirasyachting@gmail.com" target="_blank" rel="noreferrer">
+                                            <FontAwesomeIcon size="2x" icon={faEnvelope} />
+                                            <span>+90 (544) 413 63 78</span>
+                                        </a>
                                     </div>
                                 </div>
                             </div>
