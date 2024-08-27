1c252038bd791dbffc614899db567ac11192e475
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-08-27 22:14:45 +0300

    add router for services page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
+import ServicesPage from './pages/ServicesPage';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
@@ -12,6 +13,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="/services" element={<ServicesPage />} />
       </Routes>
     </Router>
   );
