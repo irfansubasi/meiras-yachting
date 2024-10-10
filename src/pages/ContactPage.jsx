23c413391667c07d32d8e7ba2176bd79a1cef960
irfansubasi <136987051+irfansubasi@users.noreply.github.com>
2024-10-10 14:00:10 +0300

    add styles

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,22 +1,22 @@
-import Nav from "../components/Nav";
-import Form from "../components/Form";
-import Footer from "../components/Footer";
+import Nav from '../components/Nav';
+import Footer from '../components/Footer';
 import '../styles/ContactPage.css';
+import ContactForm from '../components/ContactForm';
 
 export default function ContactPage() {
-    return(
-        <>
-            <Nav />
-            <main>
-                <section className="main-contact">
-                    <div className="container">
-                        <div className="form-container">
-                            <Form />
-                        </div>
-                    </div>
-                </section>
-            </main>
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Nav />
+      <main>
+        <section className="main-contact">
+          <div className="container">
+            <div className="form-container">
+              <ContactForm />
+            </div>
+          </div>
+        </section>
+      </main>
+      <Footer />
+    </>
+  );
 }
--- src/pages/RentPage.jsx
+++ src/pages/RentPage.jsx
@@ -1,12 +1,36 @@
+import { Form, FormGroup, Input, Label, Button } from 'reactstrap';
 import Header from '../components/Header';
 import Nav from '../components/Nav';
+import '../styles/RentPage.css';
 
 export default function RentPage() {
   return (
     <>
       <Nav />
       <Header page="rent" />
-      <main></main>
+      <main>
+        <div className="categories p-5 bg-white position-absolute">
+          <Form>
+            <FormGroup check inline>
+              <Input type="checkbox" />
+              <Label check>Gulet</Label>
+            </FormGroup>
+            <FormGroup check inline>
+              <Input type="checkbox" />
+              <Label check>Motoryat</Label>
+            </FormGroup>
+            <FormGroup check inline>
+              <Input type="checkbox" />
+              <Label check>Katamaran</Label>
+            </FormGroup>
+            <FormGroup check inline>
+              <Input type="checkbox" />
+              <Label check>Yelkenli</Label>
+            </FormGroup>
+            <Button>Tümünü Görüntüle</Button>
+          </Form>
+        </div>
+      </main>
     </>
   );
 }
